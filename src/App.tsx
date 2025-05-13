083243a579ab9747170c53e88b78b9cebf2d5491
Nyraa610 <135621956+Nyraa610@users.noreply.github.com>
2025-05-13 14:15:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -114,6 +114,8 @@ function App() {
                   <Route path="/register/validate" element={<RegisterValidation />} />
                   <Route path="/register/confirmation" element={<RegisterConfirmation />} />
                   <Route path="/reset-password" element={<ResetPassword />} />
+                  <Route path="/reset/password" element={<ResetPassword />} />
+                  <Route path="/reset-password" element={<ResetPassword />} />
                   <Route path="/features" element={<Features />} />
                   <Route path="/pricing" element={<Pricing />} />
                   <Route path="/unauthorized" element={<Unauthorized />} />
