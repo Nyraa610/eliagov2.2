eea729e229fa837b55e690332616ae85c78cd228
Alexandre GON <alexandre.gon@eliago.com>
2025-05-19 13:11:08 +0200

    correction config jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,7 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'babel-jest',
   },
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    }
 };
